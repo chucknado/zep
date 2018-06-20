771ab8de9d98d682a8396cfb1255e394f3713dab
chucknado <cnadeau@zendesk.com>
2018-06-20 16:17:44 -0700

    Add gitignore

--- custom/sat_ratings.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-ticket_id = 2334547707
-root = 'https://nadosolutions.zendesk.com/api/v2/'
-url = os.path.join(root, 'satisfaction_ratings', f'{ticket_id}.json')
-response = requests.get(url,  auth=('chucknado@outlook.com', 'falcon63'))
-print(response.json())
